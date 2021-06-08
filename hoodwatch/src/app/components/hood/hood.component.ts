7abcce59b2edc1f749158f187907ad98a3a03788
melissa-koi <melissawangui3@gmail.com>
2021-06-08 16:20:28 +0300

    add all hoods url

--- hoodwatch/src/app/components/hood/hood.component.ts
+++ hoodwatch/src/app/components/hood/hood.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { SearchService } from '../../../services/neighbourhood/hood-service.service'
+import { HoodServiceService } from '../../../services/neighbourhood/hood-service.service'
 
 @Component({
   selector: 'app-hood',
--- hoodwatch/src/services/neighbourhood/hood-service.service.ts
+++ hoodwatch/src/services/neighbourhood/hood-service.service.ts
@@ -2,13 +2,14 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
+const all_hoods = "https://m-hoodwatch.herokuapp.com/api/hood/"
+const update_hoods = 
+const delete_hoods = 
 
 @Injectable({
   providedIn: 'root'
 })
 export class HoodServiceService {
-
-  all_hoods :String = "https://m-hoodwatch.herokuapp.com/api/hood/"
 
   constructor(private http: HttpClient) { }
     getAllNeighbourhoods() {
