190e1a0ff6db2c7e488d1c514e794e5afed66059
melissa-koi <melissawangui3@gmail.com>
2021-06-09 04:40:53 +0300

    add urls

--- hoodwatch/src/services/business/business.service.ts
+++ hoodwatch/src/services/business/business.service.ts
@@ -1,4 +1,11 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Neighbourhood  } from 'src/app/interfaces/neighbourhood';
+
+const all_bus = "https://m-hoodwatch.herokuapp.com/api/hood/"
+const update_bus = "https://m-hoodwatch.herokuapp.com/api/hood/update/<int:pk>/"
+const delete_bus = "https://m-hoodwatch.herokuapp.com/api/hood/delete/"
 
 @Injectable({
   providedIn: 'root'
