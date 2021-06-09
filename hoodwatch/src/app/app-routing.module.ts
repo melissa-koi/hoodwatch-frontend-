3a72a235244b30d9a63d2592b101a8356cc031f0
melissa-koi <melissawangui3@gmail.com>
2021-06-09 04:35:41 +0300

    add navbar

--- hoodwatch/src/app/app-routing.module.ts
+++ hoodwatch/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BusinessComponent } from './components/business/business.component';
+import { HoodComponent } from './components/hood/hood.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'hoods',component:HoodComponent},
+  {path:'business',component:BusinessComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
