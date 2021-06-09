464e13b44310716106e3a18bef4332b1b1f43a6d
melissa-koi <melissawangui3@gmail.com>
2021-06-09 04:42:39 +0300

    add http client

--- hoodwatch/src/services/business/business.service.ts
+++ hoodwatch/src/services/business/business.service.ts
@@ -3,14 +3,19 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Neighbourhood  } from 'src/app/interfaces/neighbourhood';
 
-const all_bus = "https://m-hoodwatch.herokuapp.com/api/hood/"
-const update_bus = "https://m-hoodwatch.herokuapp.com/api/hood/update/<int:pk>/"
-const delete_bus = "https://m-hoodwatch.herokuapp.com/api/hood/delete/"
+const all_bus = "https://m-hoodwatch.herokuapp.com/api/business/"
+const update_bus = "https://m-hoodwatch.herokuapp.com/api/business/update/"
+const delete_bus = "https://m-hoodwatch.herokuapp.com/api/business/delete/"
 
 @Injectable({
   providedIn: 'root'
 })
 export class BusinessService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  // getAllBusinesses() {
+  //   console.log( this.http.get(all_bus))
+  //   return this.http.get(all_bus)
+  // }
 }
